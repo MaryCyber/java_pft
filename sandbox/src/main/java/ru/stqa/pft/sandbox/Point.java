87d8f3d5147d28fdea9d5b9a726d7ff680e5b712
MaryCyber <lady.zhannatova@yandex.ru>
2023-06-15 03:01:28 +0300

    Задание 2 (исправлено)

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.pft.sandbox;
+public class Point {
+    public double x;
+    public double y;
+    public Point (double x, double y) {
+    this.x = x;
+    this.y = y;
+    }
+    public double distance(Point p) {
+        double a = Math.pow((this.x - p.x),2);
+        double b = Math.pow((this.y - p.y),2);
+        return Math.sqrt(a+b);
+    }
+}
